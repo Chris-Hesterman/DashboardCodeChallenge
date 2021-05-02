7d38a6ea1a8bc9c2f5603efe60aa2de3808ca5e9
Chris Hesterman <chrishesterman14@gmail.com>
2021-05-01 17:50:23 -0700

    Fix some docker issues. Still learning about it.
    Paid some dues today experimenting and learning.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 // const router = require('./routes.js');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../build')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(router);
 
+// app.use(router);
 app.get('/', (req, res) => {
-  res.status(200).send('You got it!');
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.get('/test', (req, res) => {
+  res.status(200).send('You got it');
 });
 
 module.exports = app;
