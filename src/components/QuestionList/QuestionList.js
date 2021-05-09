d20f91c9710865fd8213585c15ca9049d22a6e18
Chris Hesterman <chrishesterman14@gmail.com>
2021-05-08 17:11:57 -0700

    Implement base editing form for questions and answers on a given page
    form fields are pre-filled with fetched data for a page.
    must now implement sending data back.

--- src/components/QuestionList/QuestionList.js
+++ src/components/QuestionList/QuestionList.js
@@ -1,12 +1,12 @@
 import { StyledDiv, StyledTitle } from './QuestionList.Styles';
 import { generateQuestions } from '../../helpers';
 
-const QuestionList = ({ questions, answers }) => {
+const QuestionList = ({ questions, answers, page }) => {
   const questionBoxes = generateQuestions(questions, answers);
 
   return (
     <StyledDiv>
-      <StyledTitle>Page 3</StyledTitle>
+      <StyledTitle>Page {page}</StyledTitle>
       <div>{questionBoxes}</div>
     </StyledDiv>
   );
